9322ac1a41b7032aeca2d2b6ce32cfb664eac2b6
saesak <matthewcho2000@gmail.com>
2021-11-23 18:13:58 +0900

    asdfas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import Container from "./components/container";
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 // sentence / saying / note collector can remove or add
 // chronologically ordered, will add more features later 
